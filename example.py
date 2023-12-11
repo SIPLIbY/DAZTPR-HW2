341647e89b6c9a58ab8db1311fc9d4064265e36c
SIPLIbY <oki_cs_228@mail.ru>
2023-12-11 17:42:00 +0700

    refactoring

--- example.py
+++ example.py
@@ -1,117 +1,88 @@
-#############################
-# the code is taken from the examples section for the MIP package (https://docs.python-mip.com/en/latest/examples.html)
-# supplement the example code with your own one below
-
 from itertools import product
 from sys import stdout as out
 from mip import Model, xsum, minimize, BINARY, CBC
 
-# names of places to visit
 places = ['Antwerp', 'Bruges', 'C-Mine', 'Dinant', 'Ghent',
-          'Grand-Place de Bruxelles', 'Hasselt', 'Leuven',
-          'Mechelen', 'Mons', 'Montagne de Bueren', 'Namur',
-          'Remouchamps', 'Waterloo']
+            'Grand-Place de Bruxelles', 'Hasselt', 'Leuven',
+            'Mechelen', 'Mons', 'Montagne de Bueren', 'Namur',
+            'Remouchamps', 'Waterloo']
 
 # distances in an upper triangular matrix
 dists = [[83, 81, 113, 52, 42, 73, 44, 23, 91, 105, 90, 124, 57],
-         [161, 160, 39, 89, 151, 110, 90, 99, 177, 143, 193, 100],
-         [90, 125, 82, 13, 57, 71, 123, 38, 72, 59, 82],
-         [123, 77, 81, 71, 91, 72, 64, 24, 62, 63],
-         [51, 114, 72, 54, 69, 139, 105, 155, 62],
-         [70, 25, 22, 52, 90, 56, 105, 16],
-         [45, 61, 111, 36, 61, 57, 70],
-         [23, 71, 67, 48, 85, 29],
-         [74, 89, 69, 107, 36],
-         [117, 65, 125, 43],
-         [54, 22, 84],
-         [60, 44],
-         [97],
-         []]
+        [161, 160, 39, 89, 151, 110, 90, 99, 177, 143, 193, 100],
+        [90, 125, 82, 13, 57, 71, 123, 38, 72, 59, 82],
+        [123, 77, 81, 71, 91, 72, 64, 24, 62, 63],
+        [51, 114, 72, 54, 69, 139, 105, 155, 62],
+        [70, 25, 22, 52, 90, 56, 105, 16],
+        [45, 61, 111, 36, 61, 57, 70],
+        [23, 71, 67, 48, 85, 29],
+        [74, 89, 69, 107, 36],
+        [117, 65, 125, 43],
+        [54, 22, 84],
+        [60, 44],
+        [97],
+        []]
 
 # number of nodes and list of vertices
 n, V = len(dists), set(range(len(dists)))
 
 # distances matrix
 c = [[0 if i == j
-      else dists[i][j-i-1] if j > i
-      else dists[j][i-j-1]
-      for j in V] for i in V]
-
-model = Model(solver_name=CBC)
-
-# binary variables indicating if arc (i,j) is used on the route or not
-x = [[model.add_var(var_type=BINARY) for j in V] for i in V]
-
-# continuous variable to prevent subtours: each city will have a
-# different sequential id in the planned route except the first one
-y = [model.add_var() for i in V]
-
-# objective function: minimize the distance
-model.objective = minimize(xsum(c[i][j]*x[i][j] for i in V for j in V))
-
-# constraint : leave each city only once
-for i in V:
-    model += xsum(x[i][j] for j in V - {i}) == 1
-
-# constraint : enter each city only once
-for i in V:
-    model += xsum(x[j][i] for j in V - {i}) == 1
-
-# subtour elimination
-for (i, j) in product(V - {0}, V - {0}):
-    if i != j:
-        model += y[i] - (n+1)*x[i][j] >= y[j]-n
-
-# optimizing
-model.optimize()
+    else dists[i][j-i-1] if j > i
+    else dists[j][i-j-1]
+    for j in V] for i in V]
 
 
-# checking if a solution was found
-if model.num_solutions:
-    out.write('route with total distance %g found: %s'
-              % (model.objective_value, places[0]))
-    nc = 0
-    while True:
-        nc = [i for i in V if x[nc][i].x >= 0.99][0]
-        out.write(' -> %s' % places[nc])
-        if nc == 0:
-            break
-    out.write('\n')
 
+def original_code():
+    model = Model(solver_name=CBC)
 
-#### YOUR CODE HERE VVVVVVVVVVVVVVVVVV
+    x = [[model.add_var(var_type=BINARY) for j in V] for i in V]
+    y = [model.add_var() for i in V]
+
+    model.objective = minimize(xsum(c[i][j]*x[i][j] for i in V for j in V))
+
+    for i in V:
+        model += xsum(x[i][j] for j in V - {i}) == 1
+
+    for i in V:
+        model += xsum(x[j][i] for j in V - {i}) == 1
+
+    for (i, j) in product(V - {0}, V - {0}):
+        if i != j:
+            model += y[i] - (n+1)*x[i][j] >= y[j]-n
+
+    model.optimize()
+
+    return model.objective_value
+
+#### YOUR CODE HERE
 
 # TASK 1
-new_model = Model(solver_name=CBC)
+def task_1():
+    model = Model(solver_name=CBC)
 
-new_x = [[new_model.add_var() for j in V] for i in V]
+    x = [[model.add_var() for j in V] for i in V]
+    y = [model.add_var() for i in V]
 
-new_y = [new_model.add_var() for i in V]
+    model.objective = minimize(xsum(c[i][j]*x[i][j] for i in V for j in V))
 
-# objective function: minimize the distance
-new_model.objective = minimize(xsum(c[i][j]*new_x[i][j] for i in V for j in V))
+    for i in V:
+        model += xsum(x[i][j] for j in V - {i}) == 1
 
-# constraint : leave each city only once
-for i in V:
-    new_model += xsum(new_x[i][j] for j in V - {i}) == 1
+    for i in V:
+        model += xsum(x[j][i] for j in V - {i}) == 1
 
-# constraint : enter each city only once
-for i in V:
-    new_model += xsum(new_x[j][i] for j in V - {i}) == 1
+    for (i, j) in product(V - {0}, V - {0}):
+        if i != j:
+            model += y[i] - (n+1)*x[i][j] >= y[j]-n
 
-# subtour elimination
-for (i, j) in product(V - {0}, V - {0}):
-    if i != j:
-        new_model += new_y[i] - (n+1)*new_x[i][j] >= new_y[j]-n
+    # optimizing
+    model.optimize()
+    
+    return model.objective_value
 
-# optimizing
-new_model.optimize()
-
-
-print('MODEL = ', model.objective_value)
-print('NEW_MODEL = ', new_model.objective_value)
-
-print(model.objective_value - new_model.objective_value)
+print("DIFFERENCE BETWEEN INTEGER AND CONTINUOUS VALUES", original_code - task_1())
 
 # TASK 2
 
